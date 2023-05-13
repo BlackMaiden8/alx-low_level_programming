83539699e584d450de6b674ddeed07a6bfbeab2f
BlackMaiden8 <frimpongednita@gmail.com>
2023-05-12 19:05:34 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Write a program that multiplies two numbers
+ * Your program should print the result of the multiplication, followed by a new line
+ * You can assume that the two numbers and result of the multiplication can be stored in an integer
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error);
+ */
+int main(int argc, char *argv[])
+{
+	int index, multiplication;
+
+	multiplication = 1;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
